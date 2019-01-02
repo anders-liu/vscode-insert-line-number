a6a4212a555e4a4e4a2bdb43eeb7b01fb9166758
Anders Liu <anders.liu@gmail.com>
2019-01-02 00:36:35 -0800

    Add placeholder test.

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,22 +1,10 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
-
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+suite("LineNumberInserter Tests", function () {
+    test("Insert line number with default settings", function () {
+        // TODO:
+        // Implement the integration tests.
+        // Don't know how to simulate user select after showing of a QuickPick list.
+        assert.ok({}, "Happy pass.");
     });
 });
